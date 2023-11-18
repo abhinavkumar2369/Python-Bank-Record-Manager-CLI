2c24a3775a28f5c386e8f934e47812563836296a
abhinavkumar2369 <abhinavkumar53062@gmail.com>
2023-11-18 23:03:06 +0530

    Write func()

--- source-code.py
+++ source-code.py
@@ -17,3 +17,21 @@ def read():
             print(d) 
     except: 
             file.close()
+
+
+
+#  [-- WRITING --]  FUNCTION
+
+def write(): 
+    import pickle 
+    file = open('User Database.dat','wb') 
+    data = {} 
+    n = int(input("How many User data you want to enter ? ")) 
+    for a in range(n): 
+        data['Employee No.'] = int(input("Enter Employee Number : ")) 
+        data['Name'] = input("Enter Employee Name : ") 
+        data['Department'] = input("Enter Department : ") 
+        data['Salary'] = int(input("Salary : ")) 
+        pickle.dump(data,file)
+        print("-"*5)
+    file.close() 
